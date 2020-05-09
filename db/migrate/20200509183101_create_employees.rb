0e1d41c7364ddbb14d6384c5de47af643829bcdb
Ubuntu <ubuntu@ip-172-31-47-95.us-east-2.compute.internal>
2020-05-09 18:45:59 +0000

    Add basic CRUD for employees

---
+++ db/migrate/20200509183101_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[5.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :age
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
