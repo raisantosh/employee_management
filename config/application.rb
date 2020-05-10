516b2d194b377b87cb03246ef2444517cfa00bb4
Santosh <santoshrai235@gmail.com>
2020-05-10 03:46:53 +0000

    Configure rails components

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module EmployeeManagement
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.schema_format = :ruby
   end
 end
