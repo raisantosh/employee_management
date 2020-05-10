ab1d8a8bd8e617ad22e96aaaf4831e4f609bd209
Santosh Rai <santoshrai235@gmail.com>
2020-05-10 19:27:31 +0000

    Add email format validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,8 @@
 class Employee < ApplicationRecord
     validates :first_name, :last_name, :email, :phone, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
     
     def full_name
         "#{first_name} #{last_name}"
